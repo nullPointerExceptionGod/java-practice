d38244b35696cb24e060cb631fda6e82bf72237d
C D <email@email.com>
2019-12-19 17:40:47 +0000

    Separated default method from main method to dedicated method

--- java-practice/src/java8/Laboratory.java
+++ java-practice/src/java8/Laboratory.java
@@ -4,22 +4,25 @@ public class Laboratory {
 
 	public static void main(String[] args) {
 		
-			// anonymous object
-			Formula formula = new Formula() {
-				
-				@Override
-				public double calculate(double num) {
-					// sqrt is a default method in the same interface
-					return (sqrt(num) + 2);
-				}
-			};
+		
+	}
+	
+	private static void defaultMethod() {
+		// anonymous object
+		Formula formula = new Formula() {
 			
-			System.out.println(formula.calculate(9));
-			System.out.println(formula.sqrt(16));
-			
-			//TODO: try to understand this output -> java8.Laboratory$1
-			System.out.println(formula.getClass());
-
+			@Override
+			public double calculate(double num) {
+				// sqrt is a default method in the same interface
+				return (sqrt(num) + 2);
+			}
+		};
+		
+		System.out.println(formula.calculate(9));
+		System.out.println(formula.sqrt(16));
+		
+		//TODO: try to understand this output -> java8.Laboratory$1
+		System.out.println(formula.getClass());
 	}
 
 }
