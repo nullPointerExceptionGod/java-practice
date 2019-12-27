b8805c851e763358ec7f51cd5854cf0a1a874c75
C D <email@email.com>
2019-12-27 17:36:43 +0000

    Testing/specialNumbersGenerator

---
+++ java-practice/src/java8/SpecialNumbersFinder.java
@@ -0,0 +1,43 @@
+package java8;
+
+public class SpecialNumbersFinder {
+
+	public static void main(String[] args) {
+
+		//specialNumbersGenerator();
+
+		System.out.println(factorial(5));
+		
+	}
+	
+	private static void specialNumbersGenerator() {
+		
+		long number = 0;
+		
+		String[] digits = String.valueOf(number).split("");
+		
+		for (int i = 0; i < digits.length; i++) {
+
+			
+			
+		}
+		
+	}
+	
+	/*
+	 * 5! = 5 * 4 * 3 * 2 * 1
+	 * 
+	 * factorial(5) > factorial(4) > ... > factorial(1)
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+	private static long factorial(long number) {
+		
+		return 0;
+		
+	}
+
+}
