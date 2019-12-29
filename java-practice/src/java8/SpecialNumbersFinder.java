b27486b55f2a8e11e25bbed9568de4936b7fd13a
Carlos Domingues <carlosd@campus.ul.pt>
2019-12-29 21:39:04 +0000

    Testing/factorial done

--- java-practice/src/java8/SpecialNumbersFinder.java
+++ java-practice/src/java8/SpecialNumbersFinder.java
@@ -1,43 +1,28 @@
 package java8;
 
+import java.math.BigInteger;
+
 public class SpecialNumbersFinder {
 
 	public static void main(String[] args) {
 
-		//specialNumbersGenerator();
+		 specialNumbersGenerator(4);
 
-		System.out.println(factorial(5));
+	}
+
+	private static void specialNumbersGenerator(int numberOfSpecialNumbers) {
+
+		
 		
 	}
-	
-	private static void specialNumbersGenerator() {
-		
-		long number = 0;
-		
-		String[] digits = String.valueOf(number).split("");
-		
-		for (int i = 0; i < digits.length; i++) {
 
-			
-			
+	private static BigInteger factorial(BigInteger number) {
+
+		if (number.equals(BigInteger.ONE)) {
+			return number;
+		} else {
+			return number.multiply(factorial(number.subtract(BigInteger.ONE)));
 		}
-		
-	}
-	
-	/*
-	 * 5! = 5 * 4 * 3 * 2 * 1
-	 * 
-	 * factorial(5) > factorial(4) > ... > factorial(1)
-	 * 
-	 * 
-	 * 
-	 */
-	
-	
-	private static long factorial(long number) {
-		
-		return 0;
-		
 	}
 
 }
